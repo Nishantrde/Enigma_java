9e6632be7d39d89793bdc024d8eecacf11646d80
Nishant Garg <nishant2024rde@gmail.com>
2026-03-01 13:01:28 +0530

    key_bord setup

---
+++ main_machine.java
@@ -0,0 +1,8 @@
+import components.key_board;
+public class main_machine{
+    public static void main(String args[]){
+        key_board kbd = new key_board();
+        System.out.println(kbd.forward('D'));
+    }
+    
+}
