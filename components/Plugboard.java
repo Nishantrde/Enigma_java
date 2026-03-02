b11a40d346dacd97c229f7910d00de712405bfcc
Nishant Garg <nishant2024rde@gmail.com>
2026-03-02 09:18:18 +0530

    Plugbord compleated

--- components/Plugboard.java
+++ components/Plugboard.java
@@ -1,22 +1,29 @@
 package components;
 
 public class Plugboard {
+    String left;
+    String right;
     Plugboard(String[] pairs){
-        String left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String right = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        left = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        right = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int A = 0;
         int B = 0;
         for(String pair : pairs){
             A = left.indexOf(pair.charAt(0));
             B = left.indexOf(pair.charAt(1));
         }
-        left = left.substring(0,A)+left.substring(B, B+1)+left.substring(A+1, B)+left.substring(A, A+1)+left.substring(B+1));
+        left = left.substring(0,A)+left.substring(B, B+1)+left.substring(A+1, B)+left.substring(A, A+1)+left.substring(B+1);
     }
-    public static void main(String args[]){
-        Plugboard plg_brd = new Plugboard(new String[]{"AX"});
-    }
+        public int forward(int signal){
+            char letter = right.charAt(signal);
+            return left.indexOf(letter);
+        }
+        public int backward(int signal){
+            char letter = left.charAt(signal);
+            return right.indexOf(letter);
+        }
 }
 
 
 
-
+// 
